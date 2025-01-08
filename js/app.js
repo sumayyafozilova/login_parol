f02317d2468b3459ab7f2ae0360ab2ee91564e43
sumayyafozilova <sfozilova571@gmail.com>
2025-01-09 00:26:09 +0300

    init

---
+++ js/app.js
@@ -0,0 +1,12 @@
+let elInput1 = document.getElementById("login")
+let elInput2 = document.getElementById("password")
+let elBtn = document.getElementById("btn")
+
+ elInput1, elInput2 = prompt('Login va Pasword kiriting')
+
+if ( login === "Sumayya" password ==="1234"){
+    elInput1, elInput2.textContent = "Login va pasol to'g'ri ekan"
+}
+else {
+    elInput1, elInput2.textContent = "Login va pasol xato ekan" 
+}
